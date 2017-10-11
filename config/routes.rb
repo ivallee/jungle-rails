e2372d8c6bfe791fd774cb00e1eca6a5bb723df6
Isaac Vallee <isaacvallee@gmail.com>
2017-10-11 23:12:48 +0000

    add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,10 @@ Rails.application.routes.draw do
     resources :categories, except: [:destroy, :update, :show, :edit]
   end
 
-  resources :users, only: [:new, :create, :destroy]
+  # Routes for user signup
+  resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
