53baa5b8cece2b3e8150c98c542c0c5a03b0c906
Isaac Vallee <isaacvallee@gmail.com>
2017-10-16 23:40:09 +0000

    fix authentication implementation in contrroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.authenticate_with_credentials(params[:password, :email])
+    @user = User.authenticate_with_credentials(params[:email], params[:password])
     if @user
       session[:user_id] = @user.id
       redirect_to '/'
