a7037802767dd165f9d5eda5b4e09fe0fab31693
Isaac Vallee <isaacvallee@gmail.com>
2017-10-17 19:22:59 +0000

    initialize home page spec

---
+++ spec/features/home_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  scenario "They see all products" do
+
+    visit root_path
+
+    save_screenshot 'homepage'
+
+  end
+
+end
