7fed8c689a280de73cb2bcfeb447304db9ba7b26
Isaac Vallee <isaacvallee@gmail.com>
2017-10-17 02:30:08 +0000

    add callback to adjust product quantity

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,16 @@ class Order < ActiveRecord::Base
   has_many :line_items
 
   monetize :total_cents, numericality: true
-
   validates :stripe_charge_id, presence: true
+
+  after_create :deduct_quantity
+
+  private
+    def deduct_quantity
+      line_items.each do |i|
+        i.product.quantity -= i.quantity
+        i.product.save
+      end
+    end
 
 end
